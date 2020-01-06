743bb871b54d0490780e3cfdd323eb87f4d187ef
严俊东 <1172482914@qq.com>
2020-01-06 09:43:55 +0800

    shark 预发布配置

--- domainTurn.js
+++ domainTurn.js
@@ -1,6 +1,7 @@
 
 module.exports = new Map([
   ['leiyafeng.shark.csjmro.com', 'testshark.csjmro.com'],
+  ['stableshark.csjmro.com', 'csj-shark-vue-rel.dev.csj361.com'],
   ['leiyafeng.tiger.csjmro.com', 'testtiger.csjmro.com'],
   ['wuj.koala.csjmro.com', 'testkoala.csjb2b.com'],
   ['127.0.0.1:7001', 'localhost:7001']
